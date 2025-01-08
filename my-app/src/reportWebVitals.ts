c5d240944bc8e77799cf01e00f25f8862b42f2f6
AmberWein <amberw.dev@gmail.com>
2025-01-08 10:48:08 +0200

    Fix paths in index.tsx

--- my-app/src/reportWebVitals.ts
+++ my-app/src/reportWebVitals.ts
@@ -1,8 +1,25 @@
-import { ReportHandler } from 'web-vitals';
+// import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
+// const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+// export default reportWebVitals;
+
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then((webVitals) => {
+      const { getCLS, getFID, getFCP, getLCP, getTTFB } = webVitals;
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
