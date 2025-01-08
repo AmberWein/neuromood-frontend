8405fdfcf8e8ca347fe2d7e5b466d3429be0d806
AmberWein <amberw.dev@gmail.com>
2025-01-08 12:17:48 +0200

    Implement a basic Dashboard component.

---
+++ my-app/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,19 @@
+// This component includes the ActionsHolder, FetchedDataContainer, and RecommendationsHolder.
+
+import React from "react";
+// import ActionsHolder from "./ActionsHolder";
+// import FetchedDataContainer from "./FetchedDataContainer";
+// import RecommendationsHolder from "./RecommendationsHolder";
+import './Dashboard.css';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard-container">
+      {/* <ActionsHolder /> */}
+      {/* <FetchedDataContainer /> */}
+      {/* <RecommendationsHolder /> */}
+    </div>
+  );
+};
+
+export default Dashboard;
